6ee209554af1a6e621de2dd033417077044529ab
Your Name <you@example.com>
2024-06-09 19:32:23 -0300

    initial commit

---
+++ styles/script.js
@@ -0,0 +1,33 @@
+async function buscaEndereco(cep) {
+var mensagemErro = document.getElementById ('erro');
+ mensagemerro.innerhtml = "";
+try {
+    var consultaCEP = await fetch (` https://viacep.com.br/ws/${cep}/json/`);
+    var consultaConvertida = await consultaCEP.json();
+    if(consultaConvertida.erro) {
+    throw Error('Cep inválido');
+    }
+
+    const bairro = document.getElementById('bairro');
+    const complemento = document.getElementById('complemento');
+    const cidade = document.getElementById('cidade');
+    const endereco = document.getElementById('endereco');
+
+
+    bairro.value= consultaConvertida.bairro;
+    complemento.value = consultaConvertida.complemento;
+    cidade.value = consultaConvertida.localidade;
+    endereco.value = consultaConvertida.logradouro;
+
+    return consultaConvertida;
+
+}
+catch (erro){
+    mensagemErro.innerHTML = '<p>cep nao existente</p>'
+}
+
+console.log(consultaConvertida);
+}
+
+var cep = document.getElementById('cep');
+cep.addEventListener('focusout', () => buscaendereco(cap.value))
